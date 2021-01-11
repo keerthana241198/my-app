f8eb3fff26ff1e6b99a84bae660a52cfda7851e8
Sadhana <keerthana.yadav92@gmail.com>
2021-01-11 19:55:18 +0530

    Dairy app

---
+++ app/src/main/java/com/example/dairyapp/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.dairyapp;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import androidx.appcompat.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import static com.example.dairyapp.Database.*;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    Adapter adapter;
+    TextView noItemText;
+    Database database;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        noItemText = findViewById(R.id.noteTitle);
+        database = new Database(this);
+        List<Note> allNotes = Database.getAllNotes();
+        recyclerView = findViewById(R.id.list);
+
+        if(allNotes.isEmpty()){
+            noItemText.setVisibility(View.VISIBLE);
+        }else {
+            noItemText.setVisibility(View.GONE);
+            displayList(allNotes);
+        }
+
+    }
+
+    private void displayList(List<Note> allNotes) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this,allNotes);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add){
+            Toast.makeText(this, "Add New Note", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this,Add.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<Note> getAllNotes = Database.getAllNotes();
+        if(getAllNotes.isEmpty()){
+            noItemText.setVisibility(View.VISIBLE);
+        }else {
+            noItemText.setVisibility(View.GONE);
+            displayList(getAllNotes);
+        }
+
+
+    }
+
+}
